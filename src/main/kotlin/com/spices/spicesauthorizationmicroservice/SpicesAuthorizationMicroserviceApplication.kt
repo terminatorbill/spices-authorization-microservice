00a769cc1f3099616cda34830bae2b12a3b4312c
Bill Soumakis <scorpion-bill@hotmail.com>
2018-05-27 21:10:00 +0300

    [SP-15]: First commit

---
+++ src/main/kotlin/com/spices/spicesauthorizationmicroservice/SpicesAuthorizationMicroserviceApplication.kt
@@ -0,0 +1,11 @@
+package com.spices.spicesauthorizationmicroservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpicesAuthorizationMicroserviceApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpicesAuthorizationMicroserviceApplication>(*args)
+}
---
+++ src/test/kotlin/com/spices/spicesauthorizationmicroservice/SpicesAuthorizationMicroserviceApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.spices.spicesauthorizationmicroservice
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class SpicesAuthorizationMicroserviceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
